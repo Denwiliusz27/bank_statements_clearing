95e2e62d39ddd69201bb15c254d9e66f44d123cd
Denwiliusz27 <danwielgosz@gmail.com>
2021-05-12 12:22:43 +0200

    add boolean value

--- mt940_cleanup.pl
+++ mt940_cleanup.pl
@@ -10,31 +10,37 @@ sub removeAttach {
     open( DATA2, "> $out_file") or die "nie moge otworzyc pliku"; #open file to write;
     
     @in = <DATA1>;
+    $copy = 0;
     
     for $i (0..$#in){
         @line = split(//, $in[$i]);  # split line into characters and put into array
         
-        if ((scalar(@line)) > 1) {
-            
-            $start =join('', @line[0..3]);
-            
-            print "$start\n";
-            
-=pod
-            if ($start eq ':20:') {
-                #while ($start ne ':86:') {
-                    
-                #}
-                
-                print  "$in[$i]";
-            } elsif ($start eq ':86:') {
-                print  "$in[$i]";            
-            } elsif ($start eq 'Sume') {
-                print  "$in[$i]";            
+        if ($copy) {  # when $copy is true then line is copied to output file
+            if ((scalar(@line)) > 3) {
+                $start =join('', @line[0..3]);            
+
+                if ($start eq ':86:') {
+                    print  "$in[$i]";            
+                } elsif ($start eq 'Sume') {
+                    print  "$in[$i]";            
+                }
             }
-=cut
+        } else {
+            
+            if ((scalar(@line)) > 3) {
+                $start =join('', @line[0..3]);            
+
+                if ($start eq ':20:') {
+                    $copy = 1;
+                    print  "\n$in[$i]";
+                }
+            }
         }
-    
+        
+        
+        
+        
+        
     }
     
     close (DATA1);
