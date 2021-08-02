5cf6b30a8d0eee4aba9cdee34f3c31d05ba0bfc4
Denwiliusz27 <danwielgosz@gmail.com>
2021-08-02 11:51:18 +0200

    error from mail fixed

--- mt940_cleanup.pl
+++ mt940_cleanup.pl
@@ -1,10 +1,13 @@
 use warnings;
 use feature 'state';
+use File::Copy qw(copy);
+
 
 sub removeAttach {
         
     $in_file = $_[0];
-    $out_file = $_[1];    
+    $out_file = $_[1];
+    $same = 0;
     
     if ($in_file eq $out_file) {
         $same = 1;
@@ -12,8 +15,8 @@ sub removeAttach {
     }
     
     
-    open( DATA1, "< $in_file") or die "Cannot open first file"; # open file to read
-    open( DATA2, "> $out_file") or die "Cannot open second file"; #open file to write;
+    open( DATA1, "< $in_file") or die "\nERROR: cannot open first file - inappropriate name of file, inappropriate or missing extension, or file doesn't exist.\n\n"; # open file to read
+    open( DATA2, "> $out_file") or die "\nERROR: cannot open second file - inappropriate name of file.\n\n"; #open file to write;
     
     @in = <DATA1>;
     $copy = 0;
@@ -31,7 +34,6 @@ sub removeAttach {
                     print DATA2 "$in[$i-1]";
                     print DATA2 "$in[$i]";
                 }else {
-                    #print "dodaje linie $i\n";
                     print DATA2 $in[$i];
                 }
             }
@@ -46,7 +48,7 @@ sub removeAttach {
 
                 if ($start eq ':20:') {
                     $copy = 1;
-                    print DATA2 "$in[$i-1]";
+                    #print DATA2 "$in[$i-1]";
                     print DATA2 "$in[$i]";
                 }
             }
@@ -57,83 +59,53 @@ sub removeAttach {
     close (DATA2);
 
     if ($same == 1){
-        rewriteFiles($in_file, $out_file);
+        
+        copy($out_file, $in_file);
+        
+        unlink($out_file);
     }
 }
 
+
 sub changeFileName(){
-  
-    #print "oba pliki takie same\n";
-    #print "$in_file\n";
-        
         
     @filename = split(//, $in_file);
-        
-        
-    #print 'nowa nazwa:', @filename;
-    #print "\n";
-        
+     
     $filename[-3] = 't';
     $filename[-2] = 'x';
     $filename[-1] = 't';
         
-    #print 'nowa nazwa:', @filename;
-    #print "\n";
-        
-    #$length = scalar(@filename);
-    #print "dlugosc: ", $length;
-        
     $out_file = join('',@filename);
-    #print 'nowiusienka nazwa: ', $out_file;
-        #$out_file = $filename
         
     return $out_file;
 }
 
 
-sub rewriteFiles() {
-    $old_file = $_[0];
-    $created_file = $_[1];
-
-    open( DATA1, "< $created_file") or die "Cannot open first file"; # open file to read
-    open( DATA2, "> $old_file") or die "Cannot open second file"; #open file to write;
-    
-    @line = <DATA1>;
-    
-    for $i (0..$#line) {
-        print DATA2 @line;
-    }
-    
-    
-    
-    close (DATA1);
-    close (DATA2);
-    
-    unlink($created_file);
-}
-
-
-
-
 sub main() {
     $subroutine = $ARGV[0];
     
+    if (scalar(@ARGV) == 0) {
+        print "\nERROR: name of method should be entered.\n\n";
+        return;
+    }
+    
     if ($subroutine ne 'removeAttach') {
-        print "Appropriate program name should be entered as first argument\n";
+        print "\nERROR: inappropriate name of method entered.\n\n";
         return;
     }
     
     if (scalar(@ARGV) < 2) {
-        print "Minimum one name of file should be entered\n";
+        print "\nERROR: minimum one name of existing file should be entered.\n\n";
         return;
     }
     elsif (scalar(@ARGV) == 2){
-        #print "podano te sama nazwe pliku\n";
         &$subroutine($ARGV[1], $ARGV[1]);
         return;
+    }elsif (scalar(@ARGV) > 3) {
+        print "\nERROR: too much arguments entered.\n\n";
+        return;
     }
-    
-    
+        
     &$subroutine($ARGV[1], $ARGV[2]);
 }
 
